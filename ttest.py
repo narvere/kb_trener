82556ac7a7e2b322de990230a4f86735f3f10a7b
Denis Hohlov <deniss.hohlov@gmail.com>
2022-10-11 11:17:10 +0300

    v 0.2

--- ttest.py
+++ ttest.py
@@ -1,4 +1,6 @@
+import random
 
-while True:
-    x = list(map(str, input().split()))
-    print(x)
+dictionary = {"r_ukaz_p": ["н", "г", "р", "о", "т", "ь"]}
+x = dictionary["r_ukaz_p"]
+sign = random.choice(x)
+print(sign)
